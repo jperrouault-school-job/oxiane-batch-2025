5bf34f8ffba8b74ac2bc7ffa88ebc8259979dcab
Jérémy <jeremy.perrouault@ascadis.fr>
2025-07-02 14:52:02 +0000

    INTRO TESTS

---
+++ formation-batch/src/test/java/fr/formation/IntroBatchConfigTest.java
@@ -0,0 +1,30 @@
+package fr.formation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBatchTest
+public class IntroBatchConfigTest {
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private Job stringJob;
+
+    @Test
+    void shouldJobComplete() throws Exception {
+        jobLauncherTestUtils.setJob(stringJob);
+
+        JobExecution execution = jobLauncherTestUtils.launchJob();
+
+        Assertions.assertEquals(ExitStatus.COMPLETED, execution.getExitStatus());
+    }
+}
