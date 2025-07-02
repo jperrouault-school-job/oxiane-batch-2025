eac3b03019c0e18457fb8c3d4ecec1e5c85e1de8
Jérémy <jeremy.perrouault@ascadis.fr>
2025-07-02 15:01:32 +0000

    INTRO TESTS

--- formation-batch/src/main/java/fr/formation/FormationBatchApplication.java
+++ formation-batch/src/main/java/fr/formation/FormationBatchApplication.java
@@ -1,5 +1,6 @@
 package fr.formation;
 
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -40,33 +41,41 @@ public class FormationBatchApplication {
             
             // jobLauncher.run(stringJob, params);
 
-            WatchService ws = FileSystems.getDefault().newWatchService();
+            new Thread(() -> {
+                try {
+                    WatchService ws = FileSystems.getDefault().newWatchService();
 
-            Path p = Paths.get("/workspace");
+                    Path p = Paths.get("/workspace");
 
-            p.register(ws, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
+                    p.register(ws, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY);
 
-            while (true) {
-                WatchKey key = ws.take();
+                    while (true) {
+                        WatchKey key = ws.take();
 
-                for (WatchEvent<?> event : key.pollEvents()) {
-                    System.out.println("Evènement = " + event.kind() + ", fichier affecté = " + event.context() + ".");
-                    
-                    // JobParameters params = new JobParametersBuilder()
-                    //     .addLong("time", System.currentTimeMillis())
-                    //     .toJobParameters()
-                    // ;
-                    
-                    // jobLauncher.run(stringJob, params);
+                        for (WatchEvent<?> event : key.pollEvents()) {
+                            System.out.println("Evènement = " + event.kind() + ", fichier affecté = " + event.context() + ".");
+                            
+                            // JobParameters params = new JobParametersBuilder()
+                            //     .addLong("time", System.currentTimeMillis())
+                            //     .toJobParameters()
+                            // ;
+                            
+                            // jobLauncher.run(stringJob, params);
+                        }
+
+                        boolean valid = key.reset();
+                        
+                        if (!valid) {
+                            System.out.println("Clé de surveillance invalide, arrêt.");
+                            break;
+                        }
+                    }
                 }
 
-                boolean valid = key.reset();
-                
-                if (!valid) {
-                    System.out.println("Clé de surveillance invalide, arrêt.");
-                    break;
+                catch (Exception e) {
+                    e.printStackTrace();
                 }
-            }
+            }).start();
         };
     }
 
--- formation-batch/src/test/java/fr/formation/IntroBatchConfigTest.java
+++ formation-batch/src/test/java/fr/formation/IntroBatchConfigTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +25,12 @@ public class IntroBatchConfigTest {
     void shouldJobComplete() throws Exception {
         jobLauncherTestUtils.setJob(stringJob);
 
-        JobExecution execution = jobLauncherTestUtils.launchJob();
+        JobParameters params = new JobParametersBuilder()
+            .addString("prefix", "theprefix")
+            .toJobParameters()
+        ;
+
+        JobExecution execution = jobLauncherTestUtils.launchJob(params);
 
         Assertions.assertEquals(ExitStatus.COMPLETED, execution.getExitStatus());
     }
