d96db35b3635fab749423c0b8bf5cd241c7628c7
Jérémy <jeremy.perrouault@ascadis.fr>
2025-07-02 09:56:21 +0000

    EXO LISTENERS

--- formation-batch/src/main/java/fr/formation/listener/DemoStepListener.java
+++ formation-batch/src/main/java/fr/formation/listener/DemoStepListener.java
@@ -1,5 +1,6 @@
 package fr.formation.listener;
 
+import org.springframework.batch.core.annotation.AfterProcess;
 import org.springframework.batch.core.annotation.OnSkipInProcess;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.stereotype.Component;
@@ -10,5 +11,10 @@ public class DemoStepListener {
     @OnSkipInProcess
     public void onSkipInProcess(String item, Exception e) {
         System.out.println("❌ Elément " + item + " zappé : " + e.getMessage());
+    }
+
+    @AfterProcess
+    public void afterProcess(String item, String result) {
+        System.out.println("📤 Après le traitement : " + item + " => " + result);
     }
 }
